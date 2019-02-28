94ea0bef4a6b165e31151b6eca3887f111944f26
helmi <helmixdjs2@gmail.com>
2019-02-28 13:42:13 +0700

    option menu

--- app/src/main/java/com/example/sapikuternak/MainActivity.java
+++ app/src/main/java/com/example/sapikuternak/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sapikuternak;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    Intent intent = new Intent(this, ScreenUser.class);
+        Intent intent = new Intent(this, ScreenUser.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.password) {
+            startActivity(new Intent(this, password.class));
+        } else if (item.getItemId() == R.id.logout) {
+            startActivity(new Intent(this, logout.class));
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/sapikuternak/logout.java
@@ -0,0 +1,4 @@
+package com.example.sapikuternak;
+
+public class logout {
+}
