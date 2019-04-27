6b2e692bb7f8e2d7d13a03b4e5f3dbceebb94b79
nauraghassani <nauragm@gmail.com>
2019-04-27 23:36:46 +0700

    hasil ternak

--- app/src/main/java/com/example/sapikuternak/HasilTernak.java
+++ app/src/main/java/com/example/sapikuternak/HasilTernak.java
@@ -10,4 +10,5 @@ public class HasilTernak extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil_ternak);
     }
+    //code here
 }
